a79958820eb351eb7af10d874e4ee91046594413
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2019-12-20 20:37:37 -0800

    added punctuation to questions

--- questions.js
+++ questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
-        question: 'JQuery is a ___ Library',
+        question: 'JQuery is a ___ Library.',
         answers: ['1. JavaScript', '2. HTML', '3. Java', '4. CSS'],
         correctAnswer: '1. JavaScript'
     },
     {
-        question: 'Arrays in JavaScript can be used to store',
+        question: 'Arrays in JavaScript can be used to store _____.',
         answers: ['1. other arrays', '2. booleans', '3. numbers and strings', '4. all of the above'],
         correctAnswer: '4. all of the above'
     },
     {
-        question: 'When a web page is loaded, the browser creates a _____ of the page',
+        question: 'When a web page is loaded, the browser creates a _____ of the page.',
         answers: ['1. Document Oriented Object', '2. Document Object Model', '3. Document Tree', '4. Document Model'],
-        correctAnswer: 'b'
+        correctAnswer: '2. Document Object Model'
     },
     {
-        question: 'Javascript uses the ___ keyword to declare a variable',
+        question: 'Javascript uses the ___ keyword to declare a variable.',
         answers: ['1. variable', '2. vari', '3. var', '4. Variable'],
-        correctAnswer: 'c'
+        correctAnswer: '3. var'
     },
     {
         question: 'Which of the following is NOT a Javascript logical operator?',
         answers: ['1. &&', '2. ||', '3. !', '4. +='],
-        correctAnswer: 'd'
+        correctAnswer: '4. +='
     }
 ];
